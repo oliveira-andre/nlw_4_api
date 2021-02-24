02b323e7572637f80903d25e84e73c6d5017134c
Andre Oliveira <desmonplays@gmail.com>
2021-02-23 22:19:58 -0300

    feat: initializing model user and initializing the uuid by constructor

---
+++ src/models/User.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
+class User {
+  @PrimaryColumn()
+  readonly id: String;
+
+  @Column()
+  name: String;
+
+  @Column()
+  email: String;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { User };
