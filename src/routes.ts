751e44d1cbd2357ced8efa361a8bf323833a6c5f
Andre Oliveira <desmonplays@gmail.com>
2021-02-24 20:35:18 -0300

    feat: routing post to surveys

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,13 @@ import { Router } from "express";
 const router = Router();
 
 import { UsersController } from "./controllers/UsersController";
+import { SurveysController } from "./controllers/SurveysController";
 
 const usersController = new UsersController();
+const surveysController = new SurveysController();
 
 router.post("/users", usersController.create);
+
+router.post("/surveys", surveysController.create);
 
 export { router };
