2e0043ae84d0d2f53d1a9f32c54e958f5eba4ed3
Andre Oliveira <desmonplays@gmail.com>
2021-02-24 20:40:58 -0300

    feat: routing index surveys to show method on controller

--- src/routes.ts
+++ src/routes.ts
@@ -11,5 +11,6 @@ const surveysController = new SurveysController();
 router.post("/users", usersController.create);
 
 router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
 
 export { router };
