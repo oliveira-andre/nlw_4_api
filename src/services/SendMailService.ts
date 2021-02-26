cb099ee9f8e671951e0f232430772fd71673d72b
Andre Oliveira <desmonplays@gmail.com>
2021-02-26 00:19:55 -0300

    feat: compiling the view of email

--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -1,11 +1,13 @@
 import nodemailer, { Transporter } from "nodemailer";
+import fs from "fs";
+import handlebars from "handlebars";
 
 class SendMailService {
   private client: Transporter;
 
   constructor() {
     nodemailer.createTestAccount().then((account) => {
-      const transporter = nodemailer.createTransport({
+      this.client = nodemailer.createTransport({
         host: account.smtp.host,
         port: account.smtp.port,
         secure: account.smtp.secure,
@@ -14,16 +16,19 @@ class SendMailService {
           pass: account.pass
         }
       })
-
-      this.client = transporter;
     });
   }
 
-  async execute(to: string, subject: string, body: string) {
+  async execute(to: string, subject: string, variables: object, path: string) {
+    const templateFileContent = fs.readFileSync(path).toString("utf8");
+    
+    const mailTemplateParse = handlebars.compile(templateFileContent);
+    const html = mailTemplateParse(variables);
+
     const message = await this.client.sendMail({
       to,
       subject,
-      html: body,
+      html,
       from: "NPS <noreply@nps.com.br>"
     });
 
