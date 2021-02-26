b8cc85f6e8e50daac7e6a5b1e90a54ceb21f52be
Andre Oliveira <desmonplays@gmail.com>
2021-02-26 13:10:20 -0300

    feat: validating user fields with yup

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,16 +1,28 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
+import * as yup from "yup";
 
 import { UsersRepository } from "../repositories/UsersRepository";
 
 class UsersController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch(err) {
+      return response.status(422).json({ error: err })
+    }
+
     const usersRepository = getCustomRepository(UsersRepository);
     const userAlreadyExists = await usersRepository.findOne({ email });
 
     if (userAlreadyExists) {
-      return response.status(322).json({ error: "User already exists" });
+      return response.status(422).json({ error: "User already exists" });
     }
 
     const user = usersRepository.create({ name, email });
