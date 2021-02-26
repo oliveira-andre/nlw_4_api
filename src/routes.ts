495ab538d068d54fe606ffebd33b9c7a763dc035
Andre Oliveira <desmonplays@gmail.com>
2021-02-26 12:32:31 -0300

    feat: routing to answer controller

--- src/routes.ts
+++ src/routes.ts
@@ -5,10 +5,12 @@ const router = Router();
 import { UsersController } from "./controllers/UsersController";
 import { SurveysController } from "./controllers/SurveysController";
 import { SendMailController } from "./controllers/SendMailController";
+import { AnswerController } from "./controllers/AnswerController";
 
 const usersController = new UsersController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
 
 router.post("/users", usersController.create);
 
@@ -16,5 +18,7 @@ router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
 
 router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answerController.execute);
 
 export { router };
