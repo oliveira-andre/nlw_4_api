37ab56c92f471fb671ee90f27fd57eea48e25597
Andre Oliveira <desmonplays@gmail.com>
2021-02-25 22:41:12 -0300

    feat: routing to send mail

--- src/routes.ts
+++ src/routes.ts
@@ -4,13 +4,17 @@ const router = Router();
 
 import { UsersController } from "./controllers/UsersController";
 import { SurveysController } from "./controllers/SurveysController";
+import { SendMailController } from "./controllers/SendMailController";
 
 const usersController = new UsersController();
 const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
 
 router.post("/users", usersController.create);
 
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
+
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
