72ab7534d8e66a56248af5a2ec7d92b5d1414e34
Andre Oliveira <desmonplays@gmail.com>
2021-02-26 13:33:08 -0300

    feat: rendering errors when throw some exception error

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
 import 'reflect-metadata';
 import createConnection from './database';
 import { router } from "./routes";
+import { AppError } from "./errors/AppError";
 
 createConnection();
-import express from 'express';
+import express, { NextFunction, Response, Request } from "express";
+import "express-async-errors";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({ error: err.message });
+  }
+
+  return response.status(500).json({ error: "internal server error" });
+});
 
 export { app };
